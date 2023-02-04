805b9fa6b0c3ebaccdff2f9e5d3a0361b414ac9c
Rushikesh Sutrave <68685254+rsutrave1@users.noreply.github.com>
2023-02-04 14:19:06 -0500

    Add files via upload

---
+++ provider.tf
@@ -0,0 +1,9 @@
+provider "azurerm" {
+  features {}
+
+  subscription_id = "22fc086f-29cd-423e-9b34-a2ec99974048"
+  client_id       = "5c54c680-193f-4a02-9f72-6162136a239b"
+  client_secret   = "tVk8Q~LGVm4WLIKqOOjZfeZlXsGE7pEJGojO6cea"
+  tenant_id       = "f5934656-d4e0-401c-9b17-55ac8037b4f2"
+}
+
